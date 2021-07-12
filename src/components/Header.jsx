4538f6ffa328fe1e39d997b0a9a48773ba074d6f
plotiwitolp <plotiwitolp@gmail.com>
2021-07-12 05:06:56 +0300

    added module.css for style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import s from './Header.module.css';
+
 const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
       <img src="https://seeklogo.com/images/M/m-letter-company-logo-E2FADCFF0B-seeklogo.com.png" />
     </header>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,37 @@
+import s from "./Profile.module.css";
+
 const Profile = () => {
   return (
-    <div className="content">
-        <div className="headpic">
-          <img src="https://cdn.fishki.net/upload/post/2018/06/04/2615820/11.jpg" />
-        </div>
-        <div className="user-wrapper">
-          <img src="https://images.wallpaperscraft.ru/image/kot_morda_vzglyad_89446_1024x768.jpg" />
-          ava + description
-        </div>
-        <div>
-          My posts
-          <div>New post</div>
-          <div>
-            <div>Post 1</div>
-            <div>Post 2</div>
-          </div>
+    <div className={s.content}>
+      <div className={s.headpic}>
+        {/* <img src="https://cdn.fishki.net/upload/post/2018/06/04/2615820/11.jpg" /> */}
+      </div>
+      <div className={s.user}>
+        <img src="https://images.wallpaperscraft.ru/image/kot_morda_vzglyad_89446_1024x768.jpg" />
+        <div className="desription">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+            debitis odio. Sint asperiores quo necessitatibus. Corrupti quidem
+            eos sed nisi tenetur, velit maxime itaque doloribus amet soluta quas
+            laborum facere.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+            debitis odio. Sint asperiores quo necessitatibus. Corrupti quidem
+            eos sed nisi tenetur, velit maxime itaque doloribus amet soluta quas
+            laborum facere.
+          </p>
         </div>
       </div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div className={s.posts}>
+          <div className={s.item}>Post 1</div>
+          <div className={s.item}>Post 2</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
