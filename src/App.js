fde7d2ffcbff6511340fc1eb8e724b296f9b391c
plotiwitolp <plotiwitolp@gmail.com>
2021-07-15 18:54:06 +0300

    added new app components: Music, News, Settings; and done working links ahref by BrowserRouter/Route in App

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Route } from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
-
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <div className="content-wrapper">
-      <Profile />
-      {/* <Dialogs /> */}
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Nav />
+        <div className="content-wrapper">
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
   return (
     <div className={s.content}>
       <div className={s.headpic}>
-        {/* <img src="https://cdn.fishki.net/upload/post/2018/06/04/2615820/11.jpg" /> */}
+        <img src="https://cdn.fishki.net/upload/post/2018/06/04/2615820/11.jpg" />
       </div>
       <div className={s.user}>
         <img src="https://images.wallpaperscraft.ru/image/kot_morda_vzglyad_89446_1024x768.jpg" />
