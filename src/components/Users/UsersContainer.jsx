1d1b73569d5d0f0e1ecc80f5cceba61a4b8b5305
plotiwitolp <plotiwitolp@gmail.com>
2021-07-30 15:14:30 +0300

    fixed authorizedUserId

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -45,7 +45,7 @@ let mapStateToProps = (state) => {
 }
 
 export default compose(
-    withAuthRedirect,
+    // withAuthRedirect,
     connect(mapStateToProps, {
         follow,
         unfollow,
