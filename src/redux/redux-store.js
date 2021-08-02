19468af9c34c2582265a028e4d0fdcefd8cd1836
plotiwitolp <plotiwitolp@gmail.com>
2021-08-02 20:55:14 +0300

    added redux extension for browser

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import friendsReducer from "./friends-reducer";
@@ -18,8 +18,13 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+ //With Redux DevTools:
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store;
 
+//Without Redux DevTools:
+// let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+// window.store = store;
 export default store;
